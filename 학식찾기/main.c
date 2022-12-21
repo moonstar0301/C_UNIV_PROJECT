d3d69e0fca14118f0da35150bc6b2455ec928610
masterchoi <112082163+moonstar0301@users.noreply.github.com>
2022-12-21 16:20:36 +0900

    Update main.c

--- 학식찾기/main.c
+++ 학식찾기/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #define INF 100000
 #define NUMBERR 13
 #define NUMBER 24
+int choose1;
+int choose2;
+int menu;
 
-//전체 그래프를 초기화 합니다.
 int aa[13][13] = {
 	{0,3,INF,INF,INF,INF,INF,INF,INF,INF,3,INF,INF},
 	{INF,0,4,INF,INF,INF,INF,INF,INF,INF,3,INF,INF},
@@ -90,7 +93,7 @@ void dijkstraa(int start) {
 
 void run(void)
 {
-	int loc, i, j;
+	int loc, i, j, kk, date;
 	char* name[3] = { "정보센터식당", "복지관", "공식당" };
 	char ch[20] = "";
 	typedef struct {
@@ -114,12 +117,13 @@ void run(void)
 	printf("사범대 : 9\n");
 
 	scanf_s("%d", &loc);
+	system("CLS");
 	dijkstraa(loc);
 
 	for (i = 0; i < 3; i++)
 	{
 		emp[i].location = i;
-		emp[i].value = dd[i+10];
+		emp[i].value = dd[i + 10];
 	}
 
 	//오름차순 정렬
@@ -135,16 +139,249 @@ void run(void)
 		}
 
 	if (emp[0].location == 0)
+	{
 		strcpy(ch, name[0]);
+		menu = 1;
+	}
 	else if (emp[0].location == 1)
+	{
 		strcpy(ch, name[1]);
+		menu = 2;
+	}
 	else if (emp[0].location == 2)
+	{
 		strcpy(ch, name[2]);
-
+		menu = 3;
+	}
 
 	printf("가장 가까운 식당은 %s입니다.\n", ch);
 	printf("거리는 %dm입니다.\n", emp[0].value * 100);
 	printf("도보로 %.2lf분 소요됩니다..\n", (double)emp[0].value * 10 / 6.6);
+	printf("\n식단표를 출력하시겠습니까?(출력은 1을 입력, 출력하지 않을시 0을 입력하세요.\n");
+	scanf("%d", &kk);
+	if (kk == 1)
+	{
+		printf("\n12월 12~16일중 어떤 날짜를 선택하시겠습니까?");
+		printf("\n날짜를 입력해주세요.");
+		scanf("%d", &date);
+		if (menu == 1)
+		{
+			if (date == 12)
+			{
+				printf("12월 12일 월요일 정보센터식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("불닭크림리조또           5,500원\n");
+				printf("토마토제육치즈덮밥       5,000원\n");
+				printf("쭈꾸미비빔밥             4,000원\n");
+				printf("통등심돈가스             4,000원\n");
+				printf("라면                     2,800원\n");
+				printf("================================\n");
+			}
+			else if (date == 13)
+			{
+				printf("12월 13일 화요일 정보센터식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("동인동찜갈비             5,500원\n");
+				printf("참치마요                 5,500원\n");
+				printf("홍합수제비짬뽕           5,000원\n");
+				printf("불고기비빔밥             4,000원\n");
+				printf("통등심치즈돈가스         4.500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 14)
+			{
+				printf("12월 14일 수요일 정보센터식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("불고기쌀국수             5,500원\n");
+				printf("햄야채볶음밥             5,500원\n");
+				printf("고추장불고기덮밥         5,000원\n");
+				printf("닭불고기비빔밥           4,000원\n");
+				printf("고구마치즈돈가스         4,500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 15)
+			{
+				printf("12월 15일 목요일 정보센터식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("목살에그스테이크         5,500원\n");
+				printf("메추리알품은찜닭         5,500원\n");
+				printf("비빔만두                 5,500원\n");
+				printf("오삼비빔밥               4,000원\n");
+				printf("왕돈가스                 5,500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 16)
+			{
+				printf("12월 16일 금요일 정보센터식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("제육치즈돌솥             5,000원\n");
+				printf("봉골레파스타             5,000원\n");
+				printf("참치비빔밥               4,000원\n");
+				printf("치즈왕돈가스             5,500원\n");
+				printf("어묵우동밥               3,800원\n");
+				printf("================================\n");
+			}
+		}
+
+		else if (menu == 2)
+		{
+			if (date == 12)
+			{
+				printf("12월 12일 월요일 복지관 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("육회비빔밥               5,500원\n");
+				printf("등심돈가스               5,000원\n");
+				printf("참치덮밥                 5,000원\n");
+				printf("돼지고기김치볶음밥       5,000원\n");
+				printf("프리미엄삼겹돼지국밥     6,000원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 13)
+			{
+				printf("12월 13일 화요일 복지관 식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("파스타                   5,500원\n");
+				printf("고르곤졸라피자           5,500원\n");
+				printf("청양풍삼겹살덮밥         4,000원\n");
+				printf("참치비빔밥               4,000원\n");
+				printf("치즈돈가스               4,500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 14)
+			{
+				printf("12월 14일 수요일 복지관 식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("불고기필라프             5,000원\n");
+				printf("허니브레드               5,000원\n");
+				printf("카레닭볶음덮밥           4,000원\n");
+				printf("고구마돈가스             4,500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 15)
+			{
+				printf("12월 15일 목요일 복지관 식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("참치마요김치볶음밥       5,500원\n");
+				printf("콘새우브레드             5,500원\n");
+				printf("오므라이스               4,000원\n");
+				printf("복돈이                   5,500원\n");
+				printf("치즈복돈이               5,500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 16)
+			{
+				printf("12월 16일 금요일 복지관 식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("갈릭목살필라프           5,500원\n");
+				printf("만두그라탕               5,500원\n");
+				printf("마라샹궈덮밥             4,500원\n");
+				printf("통등심돈가스             4,000원\n");
+				printf("라면                     2,800원\n");
+				printf("================================\n");
+			}
+		}
+		else if (menu == 3)
+		{
+
+			if (date == 12)
+			{
+				printf("12월 12일 월요일 공식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("육회비빔밥               5,500원\n");
+				printf("등심돈가스               5,000원\n");
+				printf("참치덮밥                 5,000원\n");
+				printf("돼지고기김치볶음밥       5,000원\n");
+				printf("프리미엄삼겹돼지국밥     6,000원\n");
+				printf("================================\n");
+			}
+
+
+			else if (date == 13)
+			{
+				printf("12월 13일 화요일 공식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("쟁반수육                 5,500원\n");
+				printf("땡초우동                 5,000원\n");
+				printf("돈가스마요               5,000원\n");
+				printf("훈제오리덮밥             5,500원\n");
+				printf("특돼지국밥               5,000원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 14)
+			{
+				printf("12월 14일 수요일 공식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("치킨마요                 5,000원\n");
+				printf("마제덮밥                 5,000원\n");
+				printf("된장찌개정식             4,500원\n");
+				printf("떡라면                   3,000원\n");
+				printf("만두라면                 3,500원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 15)
+			{
+				printf("12월 15일 목요일 공식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("오므라이스               4,500원\n");
+				printf("소고기국밥               4,500원\n");
+				printf("바지락순두부찌개정식     5,000원\n");
+				printf("최루탄라면               3,500원\n");
+				printf("삼겹살김치전골(2인)     11,000원\n");
+				printf("================================\n");
+			}
+
+			else if (date == 16)
+			{
+				printf("12월 16일 금요일 공식당 메뉴판입니다 \n");
+				printf("\n");
+				printf("\n메뉴 \n");
+				printf("================================\n");
+				printf("불돈쏘야덮밥             5,000원\n");
+				printf("양푼이돈육양념찜         5,500원\n");
+				printf("꼬마부대찌개             5,000원\n");
+				printf("뚝배기알밥               4,500원\n");
+				printf("양푼이비빔밥             5,000원\n");
+				printf("================================\n");
+			}
+		}
+	}
 }
 
 //가장 최소 거리를 가지는 정점을 반환합니다.
@@ -219,6 +456,7 @@ void run2(void)
 	printf("체육관 : 13\n");
 
 	scanf_s("%d", &loc);
+	system("CLS");
 	dijkstra(loc);
 
 	for (i = 0; i < 10; i++)
@@ -265,15 +503,148 @@ void run2(void)
 	printf("도보로 %.2lf분 소요됩니다..\n", (double)emp[0].value * 10 / 6.6);
 }
 
-int main(void) {
+struct login
+{
+	char firstname[100];
+	char lastname[20];
+	char id[20];
+	char password[20];
+};
+
+registration()
+{
+	char id[20];
+	FILE* log;
+	log = fopen("login12.txt", "at");
+	struct login L;
+
+	while (1)
+	{
+		printf("성을 입력해 주세요: ");
+		scanf("%s", L.firstname);
+		if (!strcmp(L.firstname, "X"))
+		{
+			break;
+		}
+		printf("이름을 입력해 주세요: ");
+		scanf("%s", L.lastname);
+		printf("아이디를 입력해 주세요: ");
+		scanf("%s", L.id);
+		printf("비밀번호를 입력해 주세요: ");
+		scanf("%s", L.password);
+		fprintf(log, "%s %s %s %s\n", L.firstname, L.lastname, L.id, L.password);
+		printf("계정이 생성되었습니다. 바로 로그인 하시려면 다음 화면에서 'X'를 눌러 주세요.\n");
+	}
+
+	fclose(log);
+}
+
+int login()
+{
+	char id[20], password[20];
+	FILE* log;
+	log = fopen("login12.txt", "r");
+	struct login L;
+	int i, k;
+
+	printf("ID: ");
+	scanf("%s", id);
+	printf("Password: ");
+	scanf("%s", password);
+
+	k = 0;
+	while (fscanf(log, "%*s%*s%s%s", L.id, L.password) != EOF)
+	{
+		if (strcmp(L.id, id) == 0 && strcmp(L.password, password) == 0)
+		{
+			k = 1;
+			break;
+		}
+	}
+	fclose(log);
+
+	if (k == 1)
+	{
+		printf("접속이 완료되었습니다.\n");
+		system("CLS");
+		return;
+	}
+	else
+	{
+		printf("로그인에 실패했습니다. 다시 시도하려면 1, 종료하시려면 2를 눌러 주세요\n");
+	}
+
+	scanf("%d", &choose1);
+	if (choose1 == 1) {
+		printf("ID:\n");
+		scanf("%s", &id);
+
+		printf("Password:\n");
+		scanf("%s", &password);
+
+		if (strcmp(L.id, id) == 0 && strcmp(L.password, password) == 0)
+		{
+			printf("접속이 완료되었습니다.");
+			return;
+		}
+
+
+		else {
+			printf("로그인에 실패했습니다. 다시 시도하려면 1, 종료하시려면 2를 눌러 주세요\n");
+			scanf("%d", &choose2);
+			if (choose2 == 1) {
+				printf("ID:\n");
+				scanf("%s", &id);
+				printf("Password:\n");
+				scanf("%s", &password);
+				if (strcmp(L.id, id) == 0 && strcmp(L.password, password) == 0) {
+					printf("로그인에 성공했습니다.");
+				}
+				else
+					printf("횟수 초과로 접근이 거부되었습니다. 프로그램을 종료하겠습니다.");
+			}
+
+			else
+				printf("프로그램이 종료되었습니다.");
+		}
+
+
+
+
+	}
+
+
+
+	return k;
+
+}
+
+void main()
+{
+	int choose;
+
+	printf("회원가입은 '1', 로그인은 '2'를 입력해 주세요\n");
+	scanf("%d", &choose);
+	if (choose == 1)
+	{
+		system("CLS");
+		registration();
+	}
+
+	printf("ID와 Password를 입력해 주세요.");
+	getch();
+	system("cls");
+	login();
+
 	int check;
 	printf("학생식당을 찾고싶으면 1을 입력\n");
 	printf("편의점을 찾고싶으면 2을 입력하세요.\n");
 	scanf_s("%d", &check);
+	system("CLS");
 	if (check == 1)
 		run();
 	else if (check == 2)
 		run2();
 
 	return 0;
-} 
+}
